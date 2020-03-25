b177e8c1a0fcc3982d99d0f1e1ffb5978fc33b11
andydlindsay@gmail.com <andydlindsay@gmail.com>
2020-03-25 09:52:41 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 8765;
+const users = require('./data/users.json');
+const posts = require('./data/posts.json');
+
+app.use(express.static('public'));
+
+app.get('/api/users', (req, res) => {
+  res.json(users);
+});
+
+app.get('/api/users/:id', (req, res) => {
+  res.json(users[req.params.id]);
+});
+
+app.get('/api/posts', (req, res) => {
+  res.json(posts);
+});
+
+app.post('/api/posts', (req, res) => {
+  const post = req.body;
+  posts.push(post);
+  res.status(201).send(post);
+});
+
+app.listen(port, () => {
+  console.log(`app is listening on port ${port}`);
+});
